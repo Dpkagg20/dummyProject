d880b9af6dea5acfad48794f00f5d7695b9dd7fe
Deepak Aggarwal <deepak.aggarwal@avizva.com>
2022-02-28 20:22:48 +0530

    modified pokemon data

--- src/constants/pokemonData.jsx
+++ src/constants/pokemonData.jsx
@@ -1,26 +1,31 @@
 const pokemonDataList=[
     {
         "name" : "Mewtwo",
+        "type":"Psychic",
         "origin" : "Kanto",
         "image" : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png"
     },
     {
         "name" : "Mew",
+        "type":"Psychic",
         "origin" : "Kanto",
         "image" : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png"
     },
     {
         "name" : "Arceus",
+        "type":"Normal",
         "origin" : "Kanto",
         "image" : "https://external-preview.redd.it/mlEzwqHVAh3uKBQY7UO06wy-ycrl5lP1ynk6PAYE4Y4.png?width=640&crop=smart&auto=webp&s=0e04c63b3cec1fa2a5ab26d4800d242de15d9ec2"
     },
     {
         "name" : "Rayquaza",
+        "type":"Dragon/Flying",
         "origin" : "Kanto",
         "image" : "https://assets.pokemon.com/assets/cms2/img/pokedex/full/384.png"
     },
     {
         "name" : "Lugia",
+        "type":"Psychic/Flying",
         "origin" : "Kanto",
         "image" : "https://cdn.vox-cdn.com/thumbor/RfD4D5wEZhE_dnhDZzZXTsT0OfQ=/0x0:655x368/1400x1400/filters:focal(276x132:380x236):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/55842377/lugia_2_1011710.0.jpg"
     },
--- src/constants/utils.jsx
+++ src/constants/utils.jsx
@@ -45,7 +45,7 @@ const styles =[
 
 const logos1=["/Users/deepak.aggarwal/Documents/Work/dummy project/dummy-project/src/assets/ceo_logo.jpeg","/Users/deepak.aggarwal/Documents/Work/dummy project/dummy-project/src/assets/pokemon_logo.jpeg","/Users/deepak.aggarwal/Documents/Work/dummy project/dummy-project/src/assets/celebrilty_logo.jpeg"];
 
-const logos=["https://raw.githubusercontent.com/Dpkagg20/dummyProject/e16464ec1698560462aba218d4e4e62f691aa42d/src/assets/ceo_logo.jpeg","",""]
+const logos=["https://raw.githubusercontent.com/Dpkagg20/dummyProject/e16464ec1698560462aba218d4e4e62f691aa42d/src/assets/ceo_logo.jpeg","https://raw.githubusercontent.com/Dpkagg20/dummyProject/master/src/assets/pokemon_logo.jpeg","https://raw.githubusercontent.com/Dpkagg20/dummyProject/master/src/assets/celebrilty_logo.jpeg"]
 const titles=["CEOs","Pokemons","Celebrities"];
 
 export { styles,logos,titles}
