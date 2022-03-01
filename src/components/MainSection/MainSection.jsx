c4d7cb743fbcda1bcd1c6334c7999dfd4282fa42
Deepak Aggarwal <deepak.aggarwal@avizva.com>
2022-03-01 14:58:15 +0530

    minor changed

--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -12,7 +12,7 @@ const MainSection=(props)=> {
           <img style={{width:"300px",height:"400px"}} src={props.dataList.image}/>
           </div>
           <div>
-          <h1 style={{color:props.style.textColor}}>{props.keys[0]} :{props.dataList[props.keys[0]]}</h1>
+          <h1 style={{color:props.style.textColor}}>{props.keys[0]} : {props.dataList[props.keys[0]]}</h1>
           <h1 style={{color:props.style.textColor}}>{props.keys[1]} : {props.dataList[props.keys[1]]}</h1>
           {/* <h2>Description : {dataList[0].description}</h2> */}
           </div>
